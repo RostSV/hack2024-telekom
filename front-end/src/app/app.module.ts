16840240f71b881f73c7830ef72af00481a6d96a
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-11-23 22:31:41 +0100

    impl header

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './views/main-page/main-page.component';
 import { ChatPageComponent } from './views/chat-page/chat-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import {NgOptimizedImage} from '@angular/common';
+import { InfoBadgeComponent } from './components/info-badge/info-badge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    ChatPageComponent
+    ChatPageComponent,
+    HeaderComponent,
+    LogoComponent,
+    InfoBadgeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/components/info-badge/info-badge.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-badge',
+  templateUrl: './info-badge.component.html'
+})
+export class InfoBadgeComponent {
+
+}
