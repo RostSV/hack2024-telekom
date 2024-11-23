fa8b35daa329dd634c6ab845bad2e6b1d02d3ea1
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-11-23 20:48:57 +0100

    add routing

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from './views/main-page/main-page.component';
+import {ChatPageComponent} from './views/chat-page/chat-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'chat', component: ChatPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './views/main-page/main-page.component';
+import { ChatPageComponent } from './views/chat-page/chat-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ChatPageComponent
   ],
   imports: [
     BrowserModule,
