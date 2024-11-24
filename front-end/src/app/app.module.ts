f2a633cc410adf2f95cc17026ee06abfaca8f282
lyyllyy <dinex1336@gmail.com>
2024-11-24 05:32:48 +0100

    chat,bg,info,icon

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import {NgOptimizedImage} from '@angular/common';
 import { InfoBadgeComponent } from './components/info-badge/info-badge.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { InfoBadgeComponent } from './components/info-badge/info-badge.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/components/info-badge/info-badge.component.ts
+++ front-end/src/app/components/info-badge/info-badge.component.ts
@@ -5,5 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './info-badge.component.html'
 })
 export class InfoBadgeComponent {
+  isPopupVisible: boolean = false;
 
+  togglePopup(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+    document.body.style.overflow = this.isPopupVisible ? 'hidden' : '';
+  }
+
+  closePopup(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains('popup-overlay')) {
+      this.isPopupVisible = false;
+      document.body.style.overflow = '';
+    }
+  }
 }
