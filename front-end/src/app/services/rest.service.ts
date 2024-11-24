af3adf283f7a89ab4429d1be8911254e73dc1bbe
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-11-24 11:50:29 +0100

    finished

--- front-end/src/app/services/rest.service.ts
+++ front-end/src/app/services/rest.service.ts
@@ -8,11 +8,19 @@ import {Observable} from 'rxjs';
 export class RestService {
 
   constructor(private http: HttpClient) {}
-  compare(payload: any): Observable<any> {
+  compare(payload: any, isDraft: boolean): Observable<any> {
     const formData = new FormData();
-    formData.append('file1', payload.file1, payload.file1?.name);
-    formData.append('file2', payload.file2, payload.file2?.name);
-    formData.append('task', payload.task);
+    const emptyBlob = new Blob();
+    if(!isDraft) {
+      formData.append('file1', payload.file1, payload.file1?.name);
+      formData.append('file2', payload.file2, payload.file2?.name);
+      formData.append('task', payload.task);
+    }else{
+      formData.append('file1',emptyBlob);
+      formData.append('file2',emptyBlob);
+      formData.append('task', payload.task);
+    }
+
     return this.http.post<any>('http://localhost:8000/compare', formData);
   }
 }
