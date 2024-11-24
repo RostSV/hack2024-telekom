34acaf9d9ea00041c7553fb7d17398e1204eb46f
Rostyslav Svistula <rostyslav.svistula@student.tuke.sk>
2024-11-24 10:34:34 +0100

    fix all conflicts

--- front-end/src/app/services/rest.service.ts
+++ front-end/src/app/services/rest.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  compare(payload: any): Observable<any> {
+    const formData = new FormData();
+    formData.append('file1', payload.file1, payload.file1?.name);
+    formData.append('file2', payload.file2, payload.file2?.name);
+    formData.append('task', payload.task);
+    return this.http.post<any>('http://localhost:8000/compare', formData);
+  }
 }
